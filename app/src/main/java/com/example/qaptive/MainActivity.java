904de853067efc227d78befba8f33d56afa0fb14
ananduremanan <ananduor@gmail.com>
2022-08-18 23:07:47 +0530

    Bug Fixes

--- app/src/main/java/com/example/qaptive/MainActivity.java
+++ app/src/main/java/com/example/qaptive/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.qaptive;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -36,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!isConnected(MainActivity.this)) buildDialog(MainActivity.this).show();
+        else {
+            setContentView(R.layout.activity_main);
+        }
 
         mName = findViewById(R.id.name);
         mEmail = findViewById(R.id.email);
@@ -117,5 +125,31 @@ public class MainActivity extends AppCompatActivity {
         {
             mEmail.setError("Pleas Enter Correct Email");
         }
+    }
+    public boolean isConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) return true;
+            else return false;
+        } else
+            return false;
+    }
+    public AlertDialog.Builder buildDialog(Context c) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+
+        builder.setMessage("Something went wrong,Please try again");
+        builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        return builder;
     }
 }
