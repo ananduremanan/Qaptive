67e5bef40f9747695d973d72fb0569bd4d07b06a
ananduremanan <ananduor@gmail.com>
2022-08-18 21:12:41 +0530

    Added Splash Screen

--- app/src/main/java/com/example/qaptive/MainActivity.java
+++ app/src/main/java/com/example/qaptive/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText mName, mEmail , mUsername, mPass, mPhone;
